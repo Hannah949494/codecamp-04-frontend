ddf0e4df977e7071f022a0e5aa20661e0913ca61
Hannah Jeong <hyemin949494@gmail.com>
2021-12-07 22:00:24 +0900

    211207Update

--- class/pages/15-03-infinite-scroller/index.tsx
+++ class/pages/15-03-infinite-scroller/index.tsx
@@ -2,7 +2,7 @@ import { gql, useQuery } from "@apollo/client";
 import {
   IQuery,
   IQueryFetchBoardsArgs,
-} from "../../src/commons/types/generated/types";
+} from "../../../freeboard_frontend/src/commons/types/generated/types";
 import InfiniteScroll from "react-infinite-scroller";
 
 const FETCH_BOARDS = gql`
@@ -15,20 +15,13 @@ const FETCH_BOARDS = gql`
     }
   }
 `;
-
 export default function InfiniteScrollerPage() {
   const { data, fetchMore } = useQuery<
     Pick<IQuery, "fetchBoards">,
     IQueryFetchBoardsArgs
   >(FETCH_BOARDS);
 
-  // 1. 12페이지 요청 => 120개
-  // 2. 13페이지 요청 => 127개 (추가 7개)
-  // 3. 14페이지 요청 => 127개 (추가 0개)
-
   function onLoadMore() {
-    if (!data) return;
-
     fetchMore({
       variables: { page: Math.ceil(data?.fetchBoards.length / 10) + 1 },
       updateQuery: (prev, { fetchMoreResult }) => {
@@ -43,14 +36,27 @@ export default function InfiniteScrollerPage() {
   }
 
   return (
-    <InfiniteScroll pageStart={0} loadMore={onLoadMore} hasMore={true}>
-      {data?.fetchBoards.map((el) => (
-        <div key={el._id}>
-          <span>{el.writer}</span>
-          <span>{el.title}</span>
-          <span>{el.contents}</span>
-        </div>
-      ))}
-    </InfiniteScroll>
+    <>
+      <div>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={onLoadMore}
+          hasMore={true}
+          // loader={
+          //   <div className="loader" key={0}>
+          //     Loading ...
+          //   </div>
+          // }
+        >
+          {data?.fetchBoards.map((el) => (
+            <div key={el._id}>
+              <span>{el.writer}</span>
+              <span>{el.title}</span>
+              <span>{el.contents}</span>
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    </>
   );
 }
