4ae94c41f1da71a12196cd605627570e7c3dca32
Hannah Jeong <hyemin949494@gmail.com>
2021-12-07 20:50:05 +0900

    t

--- class/pages/15-02-inputs-spread/index.tsx
+++ class/pages/15-02-inputs-spread/index.tsx
@@ -11,38 +11,69 @@ const CREATE_BOARD = gql`
   }
 `;
 
-export default function GraphqlMutationBoard3Page() {
-  const [myInputs, setMyInputs] = useState({
+export default function GraphqlMutationBoard2Page() {
+  const [inputs, setInputs] = useState({
     writer: "",
     title: "",
     contents: "",
   });
+  // const [writer, setWriter] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [contents, setContents] = useState("");
   const [createBoard] = useMutation(CREATE_BOARD);
 
-  function onChangeMyInputs(event) {
-    setMyInputs({
-      ...myInputs,
+  // function onChangeWriter(e) {
+  //   setWriter(e.target.value);
+  // }
+
+  // function onChangeTitle(e) {
+  //   setTitle(e.target.value);
+  // }
+
+  // function onChangeContents(e) {
+  //   setContents(e.target.value);
+  // }
+
+  function onChangeInputs(event) {
+    setInputs({
+      ...inputs,
       [event.target.name]: event.target.value,
     });
   }
 
   async function zzz() {
     const result = await createBoard({
-      variables: { ...myInputs },
+      variables: { ...inputs },
     });
     console.log(result);
     console.log(result.data.createBoard.message);
+
+    alert("게시물 등록이 완료되었습니다.");
   }
 
   return (
     <>
-      작성자: <input type="text" name="writer" onChange={onChangeMyInputs} />
-      <br />
-      제목: <input type="text" name="title" onChange={onChangeMyInputs} />
-      <br />
-      내용: <input type="text" name="contents" onChange={onChangeMyInputs} />
-      <br />
-      <button onClick={zzz}>게시물 등록하기!!!</button>
+      <label>작성자</label>
+      <input
+        type="text"
+        placeholder="작성자명을 입력하세요"
+        name="writer"
+        onChange={onChangeInputs}
+      />
+      <label>제목</label>
+      <input
+        type="text"
+        placeholder="제목을 입력하세요"
+        name="title"
+        onChange={onChangeInputs}
+      />
+      <label>내용</label>
+      <textarea
+        placeholder="내용을 입력하세요"
+        name="contents"
+        onChange={onChangeInputs}
+      />
+      <button onClick={zzz}>게시물 등록하기</button>
     </>
   );
 }
